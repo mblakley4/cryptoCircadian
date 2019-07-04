781fbb0c98ddf24b8fcb66fafaaedf53dcd5869c
Mike Blakley <m.blakley@icloud.com>
2019-07-04 00:38:48 -0400

    functional app minuserror handling and CSS

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const coinGeckoURL = 'https://api.coingecko.com/api/v3/coins/markets'
 const newsAPI_Key = "8cab291695294d6f831c8f1116bd0008"
 const newsSearchURL = 'https://newsapi.org/v2/everything'
 
+let searchCoin = ''; 
+
 // function to assemble query parameters for the URL
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -17,24 +19,61 @@ function formatQueryParams(params) {
 
 function getCoinRanking() {
     console.log('getCoinRanking ran');
-
+    const rankingDisplayID = 1;
+   
     const params = {
         vs_currency: 'usd',
     }
-
     const queryString = formatQueryParams(params);
     const url = coinGeckoURL + '?' + queryString;
-
+    
     console.log('Coin Gecko URL: ' + url);
-    dataComms(url);
+
+    dataComms(url, rankingDisplayID);
 }
 
-function getCoinNews(query) {
+function displayCoinRanking(responseJson) {
+    console.log('displayCoinRanking ran');
+    console.log(responseJson);
+    emptyContainers();
+    $('#js-coinRankContainer').append(`<h2>Top 10 Coins</h2><ul id="results-list"></ul>`)
+    for (let i = 0; i < 10; i++) {
+        $('#results-list').append(
+            `<li>
+                <div class="rankNumber">${responseJson[i].market_cap_rank}</div>
+                <div class="coinTile">
+                    <div class="Row1">
+                        <img src="${responseJson[i].image}">
+                        <h3><a href="${responseJson[i].id}">${responseJson[i].id} (${responseJson[i].symbol})</a></h3>
+                        <h4>${responseJson[i].current_price} USD</h4>
+                    </div>
+                    <div class="Row2">
+                        <div class=Column1>
+                            <h5>24H%</h5>
+                            <h5>${responseJson[i].price_change_percentage_24h}</h5>
+                        </div>
+                        <div class=Column2>
+                                <h5>MARKET CAP</h5>
+                                <h5>${responseJson[i].market_cap}</h5>
+                        </div>
+                        <div class=Column3>
+                                <h5>VOLUME</h5>
+                                <h5>${responseJson[i].total_volume}</h5>
+                        </div>
+                    </div>  
+                </div>
+            </li>`
+        )
+    }
+}
+
+function getCoinNews() {
     console.log('getCoinNews ran');
+    const newsDisplayID = 2;
     const params = {
         language: 'en',
         sortBy: 'publishedAt',
-        q: query,
+        q: searchCoin,
     };
     
     const queryString = formatQueryParams(params);
@@ -47,11 +86,38 @@ function getCoinNews(query) {
             "X-Api-Key": newsAPI_Key})
         };
     
-        dataComms(url, options);
+        dataComms(url, newsDisplayID, options);
+}
+
+function displayCoinNews(responseJson) {
+    console.log('displayCoinNews ran');
+    console.log(responseJson);
+    $('#js-coinGeckoWidget').append(
+            `<script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+            <coingecko-coin-price-chart-widget currency="usd" coin-id="${searchCoin}" locale="en" height="300">
+            </coingecko-coin-price-chart-widget>`)
+    $('#js-coinNews').append(`<h2>${searchCoin} News Articles</h2><ul id="results-list"></ul>`)
+    for (let i = 0; i < 15; i++) {
+        $('#results-list').append(
+            `<li>
+                <div class="newsTile">
+                    <a target="_blank" href="${responseJson.articles[i].url}" class="article-image">
+                        <img src="${responseJson.articles[i].urlToImage}" class="tileImage">
+                    </a>
+                    <div class="newsTitle">
+                            <a target="_blank" href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a>
+                    </div>
+                    <div class="articleDescription">
+                        ${responseJson.articles[i].description}
+                    </div>
+                </div>
+            </li>`
+        )
+    }
 }
 
 // API... go-fetch
-function dataComms(url, options) {
+function dataComms(url, displayID, options) {
 fetch(url, options)
 .then(response => {
     if (response.ok) {
@@ -59,19 +125,27 @@ fetch(url, options)
     }
     throw new Error(response.statusText);
 })
-.then(responseJson => console.log(JSON.stringify(responseJson)))
-//.then(responseJson => displayResults(responseJson))
+// .then(responseJson => console.log(JSON.stringify(responseJson)))
+.then(responseJson => {
+    if (displayID === 1) {
+        displayCoinRanking(responseJson);
+    }
+    else {displayCoinNews(responseJson);
+    }
+})
 .catch(err => {
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
 });
 }
 
+//function to take user input and pass param(s) to API call functions
 function watchForm() {
 $('form').submit(event => {
     event.preventDefault();
     console.log('the form was submitted');
-    const searchCoin = $('#js-search-coin').val();
-    getCoinNews(searchCoin);
+    searchCoin = $('#js-search-coin').val();
+    emptyContainers();
+    getCoinNews();
 });
 }
 
@@ -80,3 +154,9 @@ $(watchForm);
 
 //call the main page API default display
 getCoinRanking();
+
+function emptyContainers() {
+    $('#js-coinGeckoWidget').empty()
+    $('#js-coinNews').empty()
+    $('#js-coinRankContainer').empty()
+}
